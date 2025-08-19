d40ff51aacf8b1b67afc2db328b0ebba0e91753f
unknown <valdezgabriel47@gmail.com>
2025-08-19 18:48:38 -0300

    Funcion btn contacto

---
+++ js/contacto.js
@@ -0,0 +1,11 @@
+const btnForm = document.getElementById('btn-form');
+const inputEmail = document.getElementById('Email');
+const inputNombre = document.getElementById('Nombre');
+
+
+
+
+btnForm.addEventListener('click',(e)=>{
+e.preventDefault()
+alert("Mensaje enviado con exito")
+})
