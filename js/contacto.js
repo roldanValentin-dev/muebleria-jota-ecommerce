2035396934a7fec6e9468de7079417a085357681
unknown <valdezgabriel47@gmail.com>
2025-08-20 15:40:32 -0300

    Agregado de css, header y nav

--- js/contacto.js
+++ js/contacto.js
@@ -1,9 +1,4 @@
 const btnForm = document.getElementById('btn-form');
-const inputEmail = document.getElementById('Email');
-const inputNombre = document.getElementById('Nombre');
-
-
-
 
 btnForm.addEventListener('click',(e)=>{
 e.preventDefault()
