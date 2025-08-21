6ed7a76d0144c151a705a2639792afaef1e0b2b0
unknown <valdezgabriel47@gmail.com>
2025-08-21 16:09:17 -0300

    Agregado de validacion con js

--- js/contacto.js
+++ js/contacto.js
@@ -1,6 +1,27 @@
 const btnForm = document.getElementById('btn-form');
+const inputs = document.querySelectorAll('input');
+const formulario = document.getElementsByClassName('content-form');
 
-btnForm.addEventListener('click',(e)=>{
-e.preventDefault()
-alert("Mensaje enviado con exito")
+function validacion(){ 
+    let validacion = true;
+    inputs.forEach(input =>{
+        if(input.value.trim() === ""){
+            input.style.border = 'red solid 1px'
+            validacion = false;
+
+            
+        }
+       
+    })
+    return validacion
+
+}
+
+btnForm.addEventListener('click', e=>{
+    e.preventDefault()
+    if(!validacion()){
+        alert("Error complete todos los campos")
+    }else{
+        alert("Formulario enviado con exito")
+    }
 })
